c1899bf2f9001ea4b2f41df43a337ddab7e0f849
Magrishb <bmagrish@gmail.com>
2020-11-19 11:37:20 -0600

    Updates

---
+++ precache-manifest.551d731e4b3b4e44a1296abb052d6bad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155fdd44c7bd75743c4b893260310213",
+    "url": "/Magrishb/19-Employee-Tracker-MB/index.html"
+  },
+  {
+    "revision": "bd8020f47ca1dd5ec027",
+    "url": "/Magrishb/19-Employee-Tracker-MB/static/css/main.c6001462.chunk.css"
+  },
+  {
+    "revision": "de2f9828450cc58c5ad3",
+    "url": "/Magrishb/19-Employee-Tracker-MB/static/js/2.87c333e9.chunk.js"
+  },
+  {
+    "revision": "bd8020f47ca1dd5ec027",
+    "url": "/Magrishb/19-Employee-Tracker-MB/static/js/main.c5df9f72.chunk.js"
+  },
+  {
+    "revision": "b54f593184bbfe820713",
+    "url": "/Magrishb/19-Employee-Tracker-MB/static/js/runtime~main.22161fd2.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Magrishb/19-Employee-Tracker-MB/precache-manifest.551d731e4b3b4e44a1296abb052d6bad.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Magrishb/19-Employee-Tracker-MB/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
