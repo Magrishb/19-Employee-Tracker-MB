37da4f237fe5c505aa6f6a54c5260dcec5e09822
Magrishb <bmagrish@gmail.com>
2020-11-19 09:22:48 -0600

    Added a wrapper.js to set up child funtions and added that into app.js

--- employeetrackermb/src/App.js
+++ employeetrackermb/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <div className="App">
-      
+      <Wrapper></Wrapper>
     </div>
   );
 }
